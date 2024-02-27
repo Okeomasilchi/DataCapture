a174917c239da406619e8edce50a1f496bea7aad
Okeomasilachi <okeomasilachi@gmail.com>
2024-02-27 20:30:43 +0100

    Update register and login templates and styles

--- flask_web/static/scripts/main.js
+++ flask_web/static/scripts/main.js
@@ -191,17 +191,6 @@ $(document).ready(() => {
         });
     }
 
-
-    $('#toggle-login-password').click(() => {
-        $('input[name="password"]').attr('type', $('input[name="password"]').attr('type') === 'password' ? 'text' : 'password');
-        $("#now").toggleClass('fa-eye fa-eye-slash');
-    });
-
-    $('#toggle-login-confirm_password').click(() => {
-        $('input[name="confirm_password"]').attr('type', $('input[name="confirm_password"]').attr('type') === 'password' ? 'text' : 'password');
-        $("#confirm_now").toggleClass('fa-eye fa-eye-slash');
-    });
-
     $("button.add-btn").click(() => {
         $("div.choices").append(`
             <div class="form-group">
